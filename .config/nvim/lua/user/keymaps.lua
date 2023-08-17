0cec9b86296de0cf0b709a54dc99f2d9829b84b2
sayanta <sayantasingh01@gmail.com>
2023-08-18 01:42:28 +0530

    update

---
+++ .config/nvim/lua/user/keymaps.lua
@@ -0,0 +1,92 @@
+local opts = { noremap = true, silent = true }
+--[[ local term_opts = { silent = true } ]]
+
+-- Shorten function name
+local keymap = vim.api.nvim_set_keymap
+
+-- Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " " -- meant for a specific buffer, you generally use it based on a filetype
+
+-- Modes
+-- term_mode = "t",
+-- command_mode = "c",
+
+-- Normal --
+keymap("n", "<leader>w", ":w!<cr>", opts)
+keymap("n", "<leader>q", ":q!<cr>", opts)
+keymap("n", "<leader>x", ":bdelete<cr>", opts)
+keymap("n", "<leader>h", ":nohlsearch<cr>", opts)
+keymap("n", "<leader>y", "gg<S-v>G", opts)
+
+-- Half down/up
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
+-- Search term in middle
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "NNzzv", opts)
+
+-- Edit same word all together
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], opts)
+
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+--[[ keymap("n", "<S-o>", ":BufferLineMoveNext<CR>", opts) -- fix this ]]
+--[[ keymap("n", "<S-i>", ":BufferLineMovePrev<CR>", opts) -- fix this ]]
+
+-- New buffer
+keymap("n", "<S-b>", ":enew <CR>", opts)
+
+-- Delete (not cut)
+keymap("n", "dd", '"_dd', opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", "<Esc>:m .+1<CR>==", opts)
+keymap("n", "<A-k>", "<Esc>:m .-2<CR>==", opts)
+
+-- keymap("n", "<C-v>", ":vsplit<CR>", opts)
+-- keymap("n", "<C-s>", ":split<CR>", opts)
+
+-- Insert --
+keymap("i", "jk", "<ESC>", opts)
+
+-- Move current line / block with Alt-j/k vscode.
+keymap("i", "<A-j>", "<Esc>:m .+1<CR>==gi", opts)
+keymap("i", "<A-k>", "<Esc>:m .-2<CR>==gi", opts)
+
+-- navigate within insert mode
+keymap("i", "<C-h>", "<Left>", opts)
+keymap("i", "<C-l>", "<Right>", opts)
+keymap("i", "<C-j>", "<Down>", opts)
+keymap("i", "<C-k>", "<Up>", opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap("v", "<", "<gv", opts)
+keymap("v", ">", ">gv", opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m .+1<CR>==", opts)
+keymap("v", "<A-k>", ":m .-2<CR>==", opts)
+
+-- Better paste
+keymap("x", "p", '"_dP', opts)
+
+-- Visual Block --
+keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
+keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
---
+++ .config/nvim/lua/user/settings.lua
@@ -0,0 +1,87 @@
+local options = {
+	-- showmode = false,
+	fileencoding = "utf-8",
+	writebackup = false,
+	swapfile = false,
+	hidden = true,
+	undofile = true,
+	-- updatetime = 260,
+	backup = false,
+	cursorline = true,
+	expandtab = true,
+	shiftwidth = 2,
+	tabstop = 2,
+	completeopt = { "menuone", "noselect" },
+	clipboard = "unnamedplus",
+  --[[ showcmd = false, ]]
+	-- mouse = "a",
+	confirm = true,
+	splitbelow = true,
+	splitright = true,
+	-- signcolumn = "yes",
+	smartcase = true,
+	ignorecase = true,
+	hlsearch = true,
+	-- wrap = false,
+	number = true,
+	scrolloff = 6,
+	sidescrolloff = 8,
+  --[[ spell = false, -- need to start manually ]]
+
+	-- termguicolors = true,
+	-- conceallevel = 0,
+	-- pumheight = 9,
+	-- cmdheight = 2,
+	-- showtabline = 2,
+	-- smartindent = true,
+	-- copyindent = true,  -- copy the previous indentation on autoindenting
+	-- fillchars = { eob = " " },  -- disable `~` on nonexistent lines
+	-- preserveindent = true,  -- preserve indent structure as much as possible
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+vim.opt.spelllang:append("cjk") -- disable spellchecking for asian characters (VIM algorithm does not support it)
+vim.opt.shortmess:append("c") -- don't show redundant messages from ins-completion-menu
+vim.opt.shortmess:append("sI") -- disable nvim intro
+vim.opt.whichwrap:append("<,>,[,],h,l")
+-- vim.opt.iskeyword:append("-")
+
+-- Disable builtin plugins
+-- vim.g.loaded_netrw = 1
+-- vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrwSettings = 1
+vim.g.loaded_netrwFileHandlers = 1
+vim.g.loaded_2html_plugin = 1
+vim.g.loaded_getscript = 1
+vim.g.loaded_getscriptPlugin = 1
+vim.g.loaded_gzip = 1
+vim.g.loaded_logiPat = 1
+vim.g.loaded_matchit = 1
+vim.g.loaded_tar = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_rrhelper = 1
+vim.g.loaded_spellfile_plugin = 1
+vim.g.loaded_vimball = 1
+vim.g.loaded_vimballPlugin = 1
+vim.g.loaded_zip = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_tutor = 1
+vim.g.loaded_tutor_mode_plugin = 1
+vim.g.loaded_rplugin = 1
+vim.g.loaded_syntax = 1
+vim.g.loaded_synmenu = 1
+vim.g.loaded_optwin = 1
+vim.g.loaded_compiler = 1
+vim.g.loaded_bugreport = 1
+vim.g.loaded_ftplugin = 1
+vim.g.loaded_remote_plugins = 1
+vim.g.loaded_matchparen = 1
+vim.g.loaded_shada_plugin = 1
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.lazyredraw = 1
+vim.g.editorconfig = false
